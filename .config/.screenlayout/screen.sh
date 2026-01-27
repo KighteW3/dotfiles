5d03fd56bba5eb15fd7f4e9b6f868d36c6a41e6d
emanuel.asandei <emanuel.asandei@metadrop.net>
2026-01-27 10:44:26 +0100

    Now we're using DP

--- .config/.screenlayout/screen.sh
+++ .config/.screenlayout/screen.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --off --output DP1 --off --output HDMI1 --off --output HDMI2 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+xrandr --output eDP1 --off --output DP1 --off --output HDMI1 --off --output DP1 --primary --mode 3440x1440 --rate 120.00 --pos 0x0 --rotate normal --output VIRTUAL1 --off
